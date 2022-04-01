f09d755901b337b318a676eb32fff1b38ba084e1
Jorge <jgallegosoto12@gmail.com>
2022-04-01 14:18:27 +0200

    ok

---
+++ src/noSirvePeroMeh.java
@@ -0,0 +1,10 @@
+public class noSirvePeroMeh {
+    public static void printPairs() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
